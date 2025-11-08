bde5bb9e6df398d0655092882884913ad79d267b
Chung Leong <chernyshevsky@hotmail.com>
2025-11-08 18:49:59 +0100

    Fixed argument corruption with Zig's x86 background (#4)

--- fn-binding.zig
+++ fn-binding.zig
@@ -370,12 +370,6 @@ fn Binding(comptime T: type, comptime CT: type, comptime cc: ?std.builtin.Callin
             var encoder: InstructionEncoder = .{ .output = output };
             switch (builtin.target.cpu.arch) {
                 .x86_64 => {
-                    // mov rax, address
-                    encoder.encode(.{
-                        .rex = .{},
-                        .opcode = .@"mov ax imm32/64",
-                        .imm64 = address,
-                    });
                     if (pos.stack_align_mask) |mask| {
                         // mov r11, rsp
                         encoder.encode(.{
@@ -398,6 +392,21 @@ fn Binding(comptime T: type, comptime CT: type, comptime cc: ?std.builtin.Callin
                             .imm8 = @bitCast(@as(i8, @truncate(mask))),
                         });
                     }
+                    if (builtin.zig_backend == .stage2_x86_64) {
+                        // Zig's own backend uses RAX for some reason
+                        // mov r10, rax
+                        encoder.encode(.{
+                            .rex = .{ .b = 1 },
+                            .opcode = .@"mov r/m r",
+                            .mod_rm = .{ .rm = 2, .mod = 3, .reg = 0 },
+                        });
+                    }
+                    // mov rax, address
+                    encoder.encode(.{
+                        .rex = .{},
+                        .opcode = .@"mov ax imm32/64",
+                        .imm64 = address,
+                    });
                     // mov [rsp + po.offset], rax
                     if (pos.offset >= std.math.minInt(i8) and pos.offset <= std.math.maxInt(i8)) {
                         encoder.encode(.{
@@ -430,12 +439,33 @@ fn Binding(comptime T: type, comptime CT: type, comptime cc: ?std.builtin.Callin
                         .opcode = .@"mov ax imm32/64",
                         .imm64 = trampoline_address,
                     });
-                    // jmp [rax]
-                    encoder.encode(.{
-                        .rex = .{},
-                        .opcode = .@"jmp/call/etc r/m",
-                        .mod_rm = .{ .reg = 4, .mod = 3, .rm = 0 },
-                    });
+                    if (builtin.zig_backend == .stage2_x86_64) {
+                        // mov r11, rax
+                        encoder.encode(.{
+                            .rex = .{ .b = 1 },
+                            .opcode = .@"mov r/m r",
+                            .mod_rm = .{ .rm = 3, .mod = 3, .reg = 0 },
+                        });
+                        // mov rax, r10
+                        encoder.encode(.{
+                            .rex = .{ .r = 1 },
+                            .opcode = .@"mov r/m r",
+                            .mod_rm = .{ .rm = 0, .mod = 3, .reg = 2 },
+                        });
+                        // jmp [r11]
+                        encoder.encode(.{
+                            .rex = .{ .b = 1 },
+                            .opcode = .@"jmp/call/etc r/m",
+                            .mod_rm = .{ .rm = 3, .mod = 3, .reg = 4 },
+                        });
+                    } else {
+                        // jmp [rax]
+                        encoder.encode(.{
+                            .rex = .{},
+                            .opcode = .@"jmp/call/etc r/m",
+                            .mod_rm = .{ .rm = 0, .mod = 3, .reg = 4 },
+                        });
+                    }
                 },
                 .aarch64 => {
                     if (pos.stack_align_mask) |mask| {
